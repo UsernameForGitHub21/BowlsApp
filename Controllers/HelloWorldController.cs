9eb4c912e9bb996372f2e87d3bae12abddfb3ab3
W0764405 <W0764405@ADSOL0045N-07>
2022-10-14 10:50:27 -0400

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace BowlsApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HellowWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWold/Welcome
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
